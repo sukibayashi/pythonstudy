3f4f63e423f9cbdacff3f9ea25782469268870f5
sukibayashi <tony9033bin@gmail.com>
2022-05-31 02:17:26 +0800

    2022/5/31 update

---
+++ blackhorse/07、字典/4-字典常用操作之修改數據.py
@@ -0,0 +1,6 @@
+dict1 = {'name': 'Tom', 'age': 20, 'gender': '男'}
+
+dict1['name'] = 'Lilly'
+
+dict1['school'] = 'middle school'
+print(dict1)
---
+++ blackhorse/07、字典/6-字典的遍歷值key.py
@@ -0,0 +1,13 @@
+dict1 = {'name': 'Tom', 'age': 20, 'gender': '男'}
+
+# 1. 遍歷字典的 key
+# for key in dict1.keys():
+#     print(key)
+
+# 2. 遍歷字典的 value
+# for value in dict1.values():
+# 	print(value)
+
+# 3. 遍歷字典的元素
+# for item in dict1.items():
+# 	print(item)
