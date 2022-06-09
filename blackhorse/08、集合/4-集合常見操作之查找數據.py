bbcd86ad4556c5af60c2100a47199193b9b7483a
sukibayashi <tony9033bin@gmail.com>
2022-06-09 22:53:22 +0800

    2022/6/9 update

---
+++ blackhorse/08、集合/4-集合常見操作之查找數據.py
@@ -0,0 +1,7 @@
+s1 = {10, 20, 30, 40, 50}
+
+# in 或 not in 判斷數據 10 是否存在
+print(10 in s1)
+print(60 in s1)
+print(10 not in s1)
+print(60 not in s1)
---
+++ blackhorse/09、公共操作/05-公共方法之del.py
@@ -0,0 +1,11 @@
+str1 = 'abcdefg'
+list1 = [10, 20, 30, 40, 50]
+t1 = (10, 20, 30, 40, 50)
+s1 = {10, 20, 30, 40, 50}
+dict1 = {'name': 'Tom', 'age': 18}
+
+# del 目標 或 del(目標)
+# del str1
+del(list[0])
+del(dict1['name'])
+print(dict1)
